e3edcde2e1d0f12f77d9ece68d31cac3925f0d0a
Bedrock02 <JimSteve91@gmal.com>
2017-04-09 08:22:31 -0700

    Create components that helps render metadata and render templates

---
+++ helpers/header_data.py
@@ -0,0 +1,22 @@
+header_data = {
+  'home': {
+    'desc': 'Digital Tribe is a software consulting group that seeks to build web applications for entrepreneurs who are looking to start or upgrade their digital presence',
+    'keywords': 'Digital Tribe, Software consulting, small business, black entreprenur, latino entreprenur, minority entreprenur, seo, web app, application development, webiste',
+    'title': 'Digital Tribe'
+    },
+  'about': {
+    'desc': 'Digital Tribe seeks to create a world where every business owner has a digital prescence to expand their abundance',
+    'keywords': 'Digital Tribe, websites for business, vision, mission, black entreprenur, latino entreprenur, minority entreprenur, seo, web app, application development, webiste',
+    'title': 'About Us - Digital Tribe'
+  },
+  'services': {
+    'desc': 'Digital Tribe is a software consulting group that seeks to build web applications for entrepreneurs who are looking to start or upgrade their digital presence',
+    'keywords': 'Digital Tribe, Software consulting, small business, black entreprenur, latino entreprenur, minority entreprenur, seo, web app, application development, webiste',
+    'title': 'Digital Tribe'
+  },
+  'contact': {
+    'desc': 'Digital Tribe is a software consulting group that seeks to build web applications for entrepreneurs who are looking to start or upgrade their digital presence',
+    'keywords': 'Digital Tribe, Software consulting, small business, black entreprenur, latino entreprenur, minority entreprenur, seo, web app, application development, webiste',
+    'title': 'Digital Tribe'
+  }
+}
--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import logging
 from flask import Flask
 from flask import render_template
+from helpers.header_data import header_data
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-  return render_template('home.html')
+def home():
+  return render_template('home.html', nav_item=0, header_data=header_data['home'])
 
+@app.route('/about-us.html')
+def about():
+  return render_template('about-us.html', nav_item=1, header_data=header_data['about'])
+
+@app.route('/services.html')
+def contact():
+  return render_template('services.html', nav_item=2, header_data=header_data['services'])
+
+@app.route('/contact-us.html')
+def services():
+  return render_template('contact-us.html', nav_item=3, header_data=header_data['contact'])
 
 @app.errorhandler(500)
 def server_error(e):
 # Log the error and stacktrace.
   logging.exception('An error occurred during a request.')
   return 'An internal error occurred.', 500
+
+@app.errorhandler(404)
+def server_not_found(e):
+  logging.exception('Page not found')
+  return "Oops, you have left the Tribe, the page you are looking for doesn't exist", 404
