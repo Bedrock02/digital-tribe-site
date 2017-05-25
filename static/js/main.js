abb6ada7508db5725f6ee746a32371b9e7d88237
Bedrock02 <JimSteve91@gmal.com>
2017-05-24 23:29:05 -0700

    Carousel

--- static/js/main.js
+++ static/js/main.js
@@ -1,2 +1,3 @@
 var collapse = require('../../node_modules/bootstrap-sass/assets/javascripts/bootstrap/collapse');
+var carousel = require('../../node_modules/bootstrap-sass/assets/javascripts/bootstrap/carousel');
 <!-- TODO: Need to JS to toggle the right nav items
