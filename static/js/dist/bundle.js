4a4d332f6864275b49fced5ddaba5156f1eb3736
Bedrock02 <JimSteve91@gmal.com>
2017-05-29 22:08:56 -0700

    Content to site

--- static/js/dist/bundle.js
+++ static/js/dist/bundle.js
@@ -455,4 +455,12 @@
 var collapse = require('../../node_modules/bootstrap-sass/assets/javascripts/bootstrap/collapse');
 var carousel = require('../../node_modules/bootstrap-sass/assets/javascripts/bootstrap/carousel');
 <!-- TODO: Need to JS to toggle the right nav items
+
+
+$(document).ready(function(){
+  var carousel = $('.carousel');
+  if (carousel.length != 0) {
+    carousel.carousel({'interval': 10000});
+  }
+});
 },{"../../node_modules/bootstrap-sass/assets/javascripts/bootstrap/carousel":1,"../../node_modules/bootstrap-sass/assets/javascripts/bootstrap/collapse":2}]},{},[3]);
--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,11 @@
 var collapse = require('../../node_modules/bootstrap-sass/assets/javascripts/bootstrap/collapse');
 var carousel = require('../../node_modules/bootstrap-sass/assets/javascripts/bootstrap/carousel');
 <!-- TODO: Need to JS to toggle the right nav items
+
+
+$(document).ready(function(){
+  var carousel = $('.carousel');
+  if (carousel.length != 0) {
+    carousel.carousel({'interval': 10000});
+  }
+});
